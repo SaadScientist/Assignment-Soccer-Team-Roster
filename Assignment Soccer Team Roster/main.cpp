6ba3b7e3325951e3b2334709e42fa12f8492c710
Saad Arshad <muhammadsaad@muhammads-macbook-pro.local>
2018-04-10 22:33:09 -0700

    Initial Commit

---
+++ Assignment Soccer Team Roster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment Soccer Team Roster
+//
+//  Created by Muhammad Saad on 4/10/18.
+//  Copyright © 2018 Muhammad Saad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
